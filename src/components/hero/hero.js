29e587fbd16fc5e268997eec2d99fb677fc26494
Konrad Dunski <44006045+k-dun@users.noreply.github.com>
2023-07-28 17:39:09 +0100

    Hero section complete.

--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -1,14 +1,42 @@
 import './hero.css';
 import React from 'react';
 import Image from 'next/image';
+import { FiCornerRightDown, FiArrowDown, FiFileText } from 'react-icons/fi';
 
 export default function Hero() {
 
   return (
     <>
-      <div className="bg-[#FCFAFF] min-h-screen">
-
-      </div>
+      <section className="bg-[#FCFAFF] h-screen w-full flex px-5">
+        <div className="flex flex-col md:flex-row-reverse">
+          <div className="flex justify-center">
+            <Image src="/hero-dev.png" className="pt-8" width={750} height={750} alt="Konrad Dunski - Dev For Hire" />
+          </div>
+          <div className="flex flex-col justify-start items-center mt-16">
+            <h1 className="text-3xl font-semibold text-[#202020] font-primary">FRONTEND DEVELOPER <span className="text-[#FF8080]">/</span> DESIGNER</h1>
+            <h2 className="text-lg font-medium text-[#353535] mt-4">My name is Konrad Dunski and I am interested in UX/UI design, startups, writing and all things frontend.</h2>
+          </div>
+          <div className="flex flex-row w-full mt-10">
+            <div className="w-1/2">
+              <a href="/#about" className="my-auto">
+                <button className="flex-1 w-40 btn btn-md btn-ghost shadow-sm capitalize text-[#202020] text-base font-medium bg-[#FF8080] border-2 rounded-2xl border-solid border-[#FF8080] py-2 px-6 my-auto">
+                  About Me <FiCornerRightDown />
+                </button>
+              </a>
+            </div>
+            <div className="w-1/2">
+              <a href="/Konrad_Dunski_CV.pdf" target="_blank" className="my-auto">
+                <button className="flex-1 w-40 btn btn-md btn-ghost shadow-sm capitalize text-[#202020] text-base font-medium bg-[#FCFAFF] border-2 rounded-2xl border-solid border-[#FF8080] py-2 px-6 my-auto">
+                  Resumé <FiFileText />
+                </button>
+              </a>
+            </div>
+          </div>
+          <div className="container h-48 flex flex-col justify-end items-center pb-4">
+            <a href="/#about"><FiArrowDown className="text-4xl text-[#FF8080]" /></a>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -22,12 +22,7 @@ export default function Navbar() {
         <div className="flex w-2/3 justify-end items-center">
           <div className="flex justify-end items-center">
             <div className="flex flex-row">
-              <a href="/Konrad_Dunski_CV.pdf" target="_blank" className="my-auto">
-                <button className="flex-1 btn btn-xl btn-ghost shadow-sm text-[#202020] font-primary rounded-2xl border-2 border-solid border-[#FF8080] py-2 px-6 my-auto">
-                  <span className="text-2xl">resumé</span>
-                </button>
-              </a>
-
+              
               {!menuOpen && (
                 <button className="flex-1 btn btn-lg btn-square btn-ghost" onClick={() => toggleMenu()}>
                   <Image src="/nav-burger.svg" width={24} height={24} />
@@ -39,10 +34,10 @@ export default function Navbar() {
                   <label tabIndex={0} className="flex-1 btn btn-lg btn-square btn-ghost">
                     <Image src="/nav-burger-active.svg" width={24} height={24} onClick={() => toggleMenu()} />
                   </label>
-                  <ul tabIndex={0} className="dropdown-content z-[1] p-2 shadow bg-[#FCFAFF] rounded-box w-[96vw] h-[60vh]">
-                    <li className="text-black"><a href="/#about" onClick={() => toggleMenu()}>ABOUT</a></li>
-                    <li className="text-black"><a href="/#projects" onClick={() => toggleMenu()}>PROJECTS</a></li>
-                    <li className="text-black"><a href="/#contact" onClick={() => toggleMenu()}>CONTACT</a></li>
+                  <ul tabIndex={0} className="dropdown-content z-[1] p-2 shadow bg-[#FCFAFF] rounded-box w-[100vw] h-[100vh] text-center font-primary font-semibold text-4xl text-[#202020]">
+                    <li className="mt-24 underline"><a href="/#about" onClick={() => toggleMenu()}>ABOUT</a></li>
+                    <li className="mt-6 underline"><a href="/#projects" onClick={() => toggleMenu()}>PROJECTS</a></li>
+                    <li className="mt-6 underline"><a href="/#contact" onClick={() => toggleMenu()}>CONTACT</a></li>
                   </ul>
                 </div> 
               )}
